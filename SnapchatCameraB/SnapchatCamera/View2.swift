abae0d38516254a217f331b7d8294c507ed55219
Archetapp <archetapp@gmail.com>
2015-09-02 11:52:27 -0600

    SnapchatPhotosB

---
+++ SnapchatCameraB/SnapchatCamera/View2.swift
@@ -0,0 +1,133 @@
+//
+//  View2.swift
+//  SnapchatCamera
+//
+//  Created by Jared Davidson on 8/26/15.
+//  Copyright (c) 2015 Archetapp. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class View2 : UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    @IBOutlet var cameraView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+               
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error : NSError?
+        var input = AVCaptureDeviceInput(device: backCamera, error: &error)
+        
+        if (error == nil && captureSession?.canAddInput(input) != nil){
+            
+            captureSession?.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if (captureSession?.canAddOutput(stillImageOutput) != nil){
+                captureSession?.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                cameraView.layer.addSublayer(previewLayer)
+                captureSession?.startRunning()
+                
+            }
+            
+            
+        }
+        
+    
+    }
+    @IBOutlet var tempImageView: UIImageView!
+    
+    
+    func didPressTakePhoto(){
+        
+        if let videoConnection = stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo){
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
+                (sampleBuffer, error) in
+                
+                if sampleBuffer != nil {
+                    
+                    
+                    var imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    var dataProvider  = CGDataProviderCreateWithCFData(imageData)
+                    var cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, kCGRenderingIntentDefault)
+                    
+                    var image = UIImage(CGImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.Right)
+                    
+                    self.tempImageView.image = image
+                    self.tempImageView.hidden = false
+                    
+                }
+                
+                
+            })
+        }
+        
+        
+    }
+    
+    
+    var didTakePhoto = Bool()
+    
+    func didPressTakeAnother(){
+        if didTakePhoto == true{
+            tempImageView.hidden = true
+            didTakePhoto = false
+            
+        }
+        else{
+            captureSession?.startRunning()
+            didTakePhoto = true
+            didPressTakePhoto()
+            
+        }
+        
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        didPressTakeAnother()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
